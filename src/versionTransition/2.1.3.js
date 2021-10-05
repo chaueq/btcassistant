29a2d29df0e35a6afeb07e062e2bb3f18b58d3ad
chaueq <chaueq@protonmail.com>
2021-10-06 01:34:24 +0200

    Skipping if data is already compliant with new standard

--- src/versionTransition/2.1.3.js
+++ src/versionTransition/2.1.3.js
@@ -1,7 +1,10 @@
 if(cmpVersions("2.1.3", getVersionLastSeen())) {
-  setActiveCrypto({
-    name: JSON.parse(window.localStorage.getItem('activeCrypto')),
-    lastChanged: 0,
-    paused: false
-  })
+  let current = JSON.parse(window.localStorage.getItem('activeCrypto'));
+  if(typeof current == 'string') {
+    setActiveCrypto({
+      name: current,
+      lastChanged: 0,
+      paused: false
+    });
+  }
 }
